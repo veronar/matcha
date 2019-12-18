bd694bbbacf0ceea2874f22d0e95ef36300b8c39
veronar <vesingh@student.wethinkcode.co.za>
2019-12-18 12:35:45 +0200

    google clinet id

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
 	MongoDB: process.env.MONGO_DB,
 	FacebookAppID: process.env.FB_App_ID,
 	FacebookAppSecret: process.env.FB_App_SECRET,
-	GoogleClientID: '823399441169-f2p4rajke73jctj564goo1polnc66cg7.apps.googleusercontent.com',
-	GoogleClientSecret: '7xEY1CXRpuc8mVnnuUGU7427'
+	GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+	GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
