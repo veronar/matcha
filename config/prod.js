0417d43ddcac95ef2200db7b886db29cf171c600
veronar <vesingh@student.wethinkcode.co.za>
2019-12-18 12:01:26 +0200

    fb app updated

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
 	MongoDB: process.env.MONGO_DB,
-	FacebookAppID: '594358668019140',
-	FacebookAppSecret: 'c9c03af4f0c0348467a594d161fb9531',
+	FacebookAppID: process.env.FB_App_ID,
+	FacebookAppSecret: process.env.FB_App_SECRET,
 	GoogleClientID: '823399441169-f2p4rajke73jctj564goo1polnc66cg7.apps.googleusercontent.com',
 	GoogleClientSecret: '7xEY1CXRpuc8mVnnuUGU7427'
 };
