3469f968bff1703d43b8e97ada61d4249933575a
veronar <vesingh@student.wethinkcode.co.za>
2019-12-18 11:19:30 +0200

    hroku

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-	MongoDB: 'mongodb+srv://admin:Password1@matcha-qzfmp.mongodb.net/test?retryWrites=true&w=majority',
-	FacebookAppID: '594358668019140',
-	FacebookAppSecret: 'c9c03af4f0c0348467a594d161fb9531',
-	GoogleClientID: '823399441169-f2p4rajke73jctj564goo1polnc66cg7.apps.googleusercontent.com',
-	GoogleClientSecret: '7xEY1CXRpuc8mVnnuUGU7427'
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./prod');
+} else {
+	module.exports = require('./dev');
+}
