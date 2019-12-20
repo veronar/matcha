5586f8ab121841d208cb6564a2cc46d9c029a835
veronar <vesingh@student.wethinkcode.co.za>
2019-12-20 11:22:18 +0200

    aws id & secret added

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
 	FacebookAppID: process.env.FB_App_ID,
 	FacebookAppSecret: process.env.FB_App_SECRET,
 	GoogleClientID: process.env.GOOGLE_CLIENT_ID,
-	GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+	GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	AWSAccessKeyID: process.env.AWS_ACCESS_KEY,
+	AWSAccessKeySecret: process.env.AWS_ACCESS_SECRET
 };
