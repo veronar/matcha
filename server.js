9520dfe3448a80ed5b0bd92b23010a0f7fa8f5e4
veronar <vesingh@student.wethinkcode.co.za>
2020-01-15 15:02:08 +0200

    deleting chats done

--- server.js
+++ server.js
@@ -625,6 +625,15 @@ app.get('/chats', requireLogin, (req, res) => {
 		});
 });
 
+//Delete chat
+app.get('/deleteChat/:id', requireLogin, (req, res) => {
+	Chat.deleteOne({
+		_id: req.params.id
+	}).then(() => {
+		res.redirect('/chats');
+	});
+});
+
 // get route to send smile
 app.get('/sendSmile/:id', requireLogin, (req, res) => {
 	const newSmile = {
