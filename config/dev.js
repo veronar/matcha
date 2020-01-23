6e1c467e83285ec7f91f03a90990650d2bf1369e
Verona Ramkumar <vesingh@student.wethinkcode.co.za>
2020-01-23 17:23:54 +0200

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,11 @@
 module.exports = {
-	MongoDB: 'mongodb+srv://admin:Password1@matcha-qzfmp.mongodb.net/test?retryWrites=true&w=majority',
-	FacebookAppID: '594358668019140',
-	FacebookAppSecret: 'c9c03af4f0c0348467a594d161fb9531',
-	GoogleClientID: '823399441169-f2p4rajke73jctj564goo1polnc66cg7.apps.googleusercontent.com',
-	GoogleClientSecret: '7xEY1CXRpuc8mVnnuUGU7427',
-	AWSAccessKeyID: 'AKIAYEQ7AWQFAMVZE7VI',
-	AWSAccessKeySecret: '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',
-	StripePublishableKey: "pk_test_ncC40AYgXadlNN8tY1Os2ZlX009RjxDIqz",
-	StripeSecretKey: "sk_test_ExTjTaoexZ4YK48gfh2YVyAn009MfqBhjB"
+	MongoDB: 'process.env.MONGO_DB',
+	FacebookAppID: 'process.env.FB_App_ID',
+	FacebookAppSecret: 'process.env.FB_App_SECRET',
+	GoogleClientID: 'process.env.GOOGLE_CLIENT_ID',
+	GoogleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
+	AWSAccessKeyID: 'process.env.AWS_ACCESS_KEY',
+	AWSAccessKeySecret: 'process.env.AWS_ACCESS_SECRET',
+	StripePublishableKey: "process.env.STRIPE_PUBLISHABLE_KEY",
+	StripeSecretKey: "process.env.STRIPE_SECRET_KEY"
 };
